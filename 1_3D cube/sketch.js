5e7c2b3fa62ad53de5cf3928ff59a1e22669ae40
Christophe <christophe@vandenbemden.eu>
2023-12-30 17:04:00 +0100

    simple rectangle

--- 1_3D cube/sketch.js
+++ 1_3D cube/sketch.js
@@ -1,7 +1,15 @@
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight, WEBGL);
 }
 
 function draw() {
   background(0);
+
+  noFill();
+  stroke(200, 200, 200);
+  strokeWeight(2);
+
+  orbitControl();
+  rectMode(CENTER);
+  rect(0, 0,400, 400);
 }
